d84ad9fc5eda65253d4f08d26a231771ba3b2488
Basediso <163725049+Basediso@users.noreply.github.com>
2024-03-31 13:30:38 +0300

    Create Based.py

---
+++ Based.py
@@ -0,0 +1,28 @@
+import discord
+import proje
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith("$Parola_öner"):
+        await message.channel.send(proje.x(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run("TOKEN")
